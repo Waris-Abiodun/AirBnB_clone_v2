319132adc10f79d0fc4b24916476826670526766
Waris <kazeewaris05@gmail.com>
2023-02-13 10:17:34 +0000

    addingg int to route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'
+       /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: Displays 'C' followed by the value of <text>.
+    /python/(<text>): Displays 'Python' followed by the value of <text>.
+    /number/<n>: display “n is a number” only if n is an integer
+"""
+
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hnbn():
+    """Display Hello HBNB"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hnbn():
+    """Display HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_some_text(text):
+    """Display c with some text"""
+    text = text.replace('_', ' ')
+    return 'C %s' % escape(text)
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/', strict_slashes=False, defaults={"text": "is cool"})
+def python_some_text(text):
+    """Display python with some text"""
+    text = text.replace('_', ' ')
+    return 'Python %s' % escape(text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def anumber(n):
+    """Display a number"""
+    return str(n) + ' is a number'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
